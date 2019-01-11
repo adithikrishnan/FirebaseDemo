3ab02cf46d7a8feb3b03d2039222821083333d89
Adithi Krishnan <38013049+adithikrishnan@users.noreply.github.com>
2019-01-11 21:30:25 +0530

    Update WordAdapter.java

--- src/main/java/adithi/com/example/android/myapplication/WordAdapter.java
+++ src/main/java/adithi/com/example/android/myapplication/WordAdapter.java
@@ -1,4 +1,31 @@
 package adithi.com.example.android.myapplication;
 
-public class WordAdapter {
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+public class WordAdapter extends ArrayAdapter<Word> {
+     public WordAdapter(Activity context, ArrayList<Word> words) {
+        super(context, 0, words);
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItemView = convertView;
+        if(listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item, parent, false);
+             }
+        
+        Word currentWord = getItem(position);
+
+        TextView usernameTextView = (TextView) listItemView.findViewById(R.id.user_name);
+        usernameTextView.setText(currentWord.getUsername());
+        TextView userageTextView = (TextView) listItemView.findViewById(R.id.user_age);
+        userageTextView.setText(currentWord.getUserage());
+    
+        return listItemView;
+    }
 }
