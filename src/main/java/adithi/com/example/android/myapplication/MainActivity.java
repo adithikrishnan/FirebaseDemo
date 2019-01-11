e610346a613fe56444c6cbfd36c9edac4db681d0
Adithi Krishnan <38013049+adithikrishnan@users.noreply.github.com>
2019-01-11 21:33:31 +0530

    Update MainActivity.java

--- src/main/java/adithi/com/example/android/myapplication/MainActivity.java
+++ src/main/java/adithi/com/example/android/myapplication/MainActivity.java
@@ -2,12 +2,22 @@ package adithi.com.example.android.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+        TextView username = (TextView) findViewById(R.id.user_name);
+        TextView userage = (TextView) findViewById(R.id.user_age);
+
+        
     }
+
 }
